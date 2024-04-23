8ccf326f6a694470bf33b87824114009cc863969
KazariEX <1364035137@qq.com>
2024-04-24 00:29:51 +0800

    revert: 恢复测试用例

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -2,7 +2,7 @@ import { fileURLToPath } from "node:url";
 import { describe, expect, it } from "vitest";
 import { $fetch, setup } from "@nuxt/test-utils/e2e";
 
-describe.skip("ssr", async () => {
+describe("ssr", async () => {
     await setup({
         rootDir: fileURLToPath(new URL("./fixtures/basic", import.meta.url))
     });
