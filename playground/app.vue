21ec0a0d136d69ae6f24be2fa8666459c1fb7a15
KazariEX <1364035137@qq.com>
2024-04-23 20:02:46 +0800

    test(playgroud): 添加用例

--- playground/app.vue
+++ playground/app.vue
@@ -2,15 +2,22 @@
     const gsap = useGsap();
 
     gsap.registerEffect({
+        name: "rotate",
+        effect(targets, config) {
+            return gsap.to(targets, { rotate: 360, ease: "linear", duration: 1.5, repeat: -1, ...config });
+        }
+    });
+
+    gsap.registerEffect({
         name: "fade",
-        effect: (targets, config) => {
+        effect(targets, config) {
             return gsap.to(targets, { duration: 1.5, opacity: 0, ...config });
         }
     });
 
     gsap.registerEffect({
         name: "slide-in",
-        effect: (targets, config) => {
+        effect(targets, config) {
             return gsap.from(targets, { x: -360, duration: 1.5, opacity: 0, ...config });
         }
     });
@@ -19,6 +26,7 @@
 <template>
     <div class="demo">
         <img v-gsap.to="{ rotate: 360, opacity: 1, repeat: -1 }" src="~/assets/nuxt.svg"/>
+        <img v-gsap.rotate src="~/assets/nuxt.svg"/>
         <img v-gsap.slide-in src="~/assets/nuxt.svg"/>
         <img v-gsap.fade="{ duration: 3 }" src="~/assets/nuxt.svg"/>
         <img v-gsap.fade src="~/assets/nuxt.svg"/>
@@ -28,7 +36,7 @@
 <style>
     .demo {
         display: grid;
-        grid-template-rows: repeat(4, 128px);
+        grid-template-rows: repeat(5, 128px);
         justify-content: center;
         align-items: center;
     }
